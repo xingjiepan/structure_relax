6978eea181bbf0be65b24d9aa4e3ad679c1676c3
xingjiepan <xingjiepan@gmail.com>
2018-02-12 14:00:07 -0800

    Update the score_structure.py script

--- score_structure.py
+++ score_structure.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     rosetta.core.import_pose.pose_from_file(pose, pdb_file)
 
     sfxn = rosetta.core.scoring.get_score_function()
-    sfxn.set_weight(rosetta.core.scoring.fa_dun, 0)
-    sfxn.set_weight(rosetta.core.scoring.fa_rep, 0)
+    #sfxn.set_weight(rosetta.core.scoring.fa_dun, 0)
+    #sfxn.set_weight(rosetta.core.scoring.fa_rep, 0)
     sfxn(pose)
 
     print "Total score =", pose.energies().total_energy()
